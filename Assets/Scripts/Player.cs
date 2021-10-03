b3e1ce0a474e18d82801f342af177e536120513e
Nicholas Kane <nkanedev@gmail.com>
2021-10-03 13:55:03 -0400

    updating projectile to collide with wall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,37 +43,22 @@ public class Player : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        /*
-        hit = Physics2D.BoxCast(transform.position, collider_2d.size, 0,
-                                new Vector2(0, move_delta.y), Mathf.Abs(move_delta.y * Time.deltaTime),
-                                LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(0, move_delta.y * Time.deltaTime, 0);
-        }
-        hit = Physics2D.BoxCast(transform.position, collider_2d.size, 0,
-                                new Vector2(move_delta.x, 0), Mathf.Abs(move_delta.x * Time.deltaTime),
-                                LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null) 
-        {
-            transform.Translate(move_delta.x * Time.deltaTime, 0, 0);
-        }
-        */
     }
 
     private void Fire()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity) as GameObject;
-
+            Vector3 pos = transform.position;
+            Quaternion rot = Quaternion.AngleAxis(transform.localScale.x * -90.0f, Vector3.forward);
+            Instantiate(projectilePrefab, pos, rot);
+            /*
             var velocity = projectile.GetComponent<Projectile>().GetVelocity();
-
             // Set the orientation of the arrow based on user face
             var x = transform.localScale.x;
-            projectile.transform.rotation = Quaternion.AngleAxis(x * -90, Vector3.forward);
-
+            projectile.transform.rotation = ;
             projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(x * velocity, 0);
+            */
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] int damage = 100;
-    [SerializeField] float velocity = 1f;
+    [SerializeField] float velocity = 1.0f;
+    public Rigidbody2D rig_body;
 
     public int GetDamage() { return damage; }
 
     public float GetVelocity() { return velocity; }
+
+    private void Start ()
+    {
+        LayerMask.NameToLayer("Actor");
+        rig_body = GetComponent<Rigidbody2D>();
+        rig_body.velocity = new Vector2(transform.localScale.x * velocity, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int current_layer = collision.gameObject.layer;
+        int actor = LayerMask.NameToLayer("Actor");
+        int blocking = LayerMask.NameToLayer("Blocking");
+        if (current_layer == blocking)
+        {
+            Hit();
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 
     public void Hit()
     {
