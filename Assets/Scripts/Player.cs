c75116a31ad089d85abbc313d320aedf82ed275a
Nicholas Kane <nkanedev@gmail.com>
2021-10-03 16:33:35 -0400

    Update Projectiles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,20 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] GameObject projectilePrefab;
-
     public float speed = 0.5f;
     public Rigidbody2D rig_body;
     public BoxCollider2D collider_2d;
     public Vector3 move_delta;
     public RaycastHit2D hit;
-    Animator myAnimator;
+    public Animator myAnimator;
+    public bool facing_right;
 
     private void Start()
     {
         myAnimator = GetComponent<Animator>();
         collider_2d = GetComponent<BoxCollider2D>();
         rig_body = GetComponent<Rigidbody2D>();
+        facing_right = true;
     }
 
     private void Update()
@@ -35,30 +35,20 @@ public class Player : MonoBehaviour
     private void Move()
     {
         rig_body.MovePosition(transform.position + (move_delta * speed * Time.deltaTime));
-        if (move_delta.x > 0)
+        if (move_delta.x > 0 && !facing_right)
         {
-            transform.localScale = Vector3.one;
+            Flip();
         }
-        else if (move_delta.x < 0)
+        else if (move_delta.x < 0 && facing_right)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            Flip();
         }
-        /*
-        hit = Physics2D.BoxCast(transform.position, collider_2d.size, 0,
-                                new Vector2(0, move_delta.y), Mathf.Abs(move_delta.y * Time.deltaTime),
-                                LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            transform.Translate(0, move_delta.y * Time.deltaTime, 0);
-        }
-        hit = Physics2D.BoxCast(transform.position, collider_2d.size, 0,
-                                new Vector2(move_delta.x, 0), Mathf.Abs(move_delta.x * Time.deltaTime),
-                                LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null) 
-        {
-            transform.Translate(move_delta.x * Time.deltaTime, 0, 0);
-        }
-        */
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+        facing_right = !facing_right;
     }
 
     private void Fire()
@@ -66,16 +56,6 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             myAnimator.SetTrigger("AttackWithBow");
-
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity) as GameObject;
-
-            var velocity = projectile.GetComponent<Projectile>().GetVelocity();
-
-            // Set the orientation of the arrow based on user face
-            var x = transform.localScale.x;
-            projectile.transform.rotation = Quaternion.AngleAxis(x * -90, Vector3.forward);
-
-            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(x * velocity, 0);
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] int damage = 100;
-    [SerializeField] float velocity = 1f;
+    [SerializeField] float speed = 1.0f;
+    public Rigidbody2D rig_body;
 
     public int GetDamage() { return damage; }
 
-    public float GetVelocity() { return velocity; }
+    public float GetVelocity() { return speed; }
+
+    private void Start() 
+    {
+        rig_body = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetDirection()
+    {
+        rig_body.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int current_layer = collision.gameObject.layer;
+        int actor = LayerMask.NameToLayer("Actor");
+        int blocking = LayerMask.NameToLayer("Blocking");
+        if (current_layer == blocking)
+        {
+            Hit();
+        }
+    }
 
     public void Hit()
     {
